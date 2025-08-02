c57ad0c0943e308e7199905915254a2b211faa5c
kamrul <kamrulahasun009@gmail.com>
2025-08-02 23:43:37 +0600

    Final Project

--- lib/project.dart
+++ lib/project.dart
@@ -1,10 +1,113 @@
 import 'package:flutter/material.dart';
 
 class Project extends StatelessWidget {
-  const Project({super.key});
-
+  Project({super.key});
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController salaryController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenWidth2 = MediaQuery.of(context).size.width;
+    double containerHeight = screenHeight * 0.5;
+    double containerWidth = screenWidth * 0.8;
+    double textFilld = screenWidth2 * 0.6;
+    return Scaffold(
+      appBar: AppBar(title: Text("Add Employe")),
+      body: Center(
+        child: Container(
+          height: containerHeight,
+          width: containerWidth,
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 8,
+                offset: Offset(4, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: nameController,
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: ageController,
+                keyboardType: TextInputType.number,
+
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Age',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: salaryController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Salary',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                width: textFilld,
+                child: ElevatedButton(
+                  onPressed: () {
+                    String name = nameController.text.trim();
+                    String age = ageController.text.trim();
+                    String salary = salaryController.text.trim();
+
+                    if (name.isEmpty || age.isEmpty || salary.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Field cannot be empty!'),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'Name: $name, Age: $age, Salary: $salary',
+                          ),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
